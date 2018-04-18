365cfc52a36e6959c3374ace26ac2c9f9ccaaf67
Ka Yin Chan <bengood362@gmail.com>
2018-04-19 01:48:11 +0800

    now download error will not break directly

--- blackboard_crawler.py
+++ blackboard_crawler.py
@@ -29,32 +29,38 @@ def mkdir(name):
     os.mkdir(str(name)+str(uuid.uuid4().hex[:8]))
 
 def download_file(url, path, sess):
-  if(blackboard_url not in url):
-    url = blackboard_url+url
-  resp = sess.get(url, stream=True)
-  url = urllib2.urlparse.unquote(resp.url)
-  if(platform == "darwin"):
-    url = url.encode('latin1')
-  print(str(path)+' '+str(url))
-  local_filename = urllib2.urlparse.unquote(url.split('/')[-1])
-  file_size = resp.headers['Content-Length']
-  if(int(file_size)>=1024*1024*100):
-    while(1):
-      download = raw_input("The file is around {0}MB, still download?(y/n)".format(int(file_size)/1024/1024))
-      if(download.lower() == 'y'):
-        break
-      elif(download.lower() == 'n'):
-        return local_filename
-      else:
-        print("Please input only y or n!")
-  # NOTE the stream=True parameter
-  r = resp
-  with open(os.path.join(path, local_filename.decode('utf-8')), 'wb') as f:
-    for chunk in r.iter_content(chunk_size=1024): 
-      if chunk: # filter out keep-alive new chunks
-        f.write(chunk)
-            #f.flush() commented by recommendation from J.F.Sebastian
-  return local_filename
+  try:
+    if(blackboard_url not in url):
+      url = blackboard_url+url
+    resp = sess.get(url, stream=True)
+    url = urllib2.urlparse.unquote(resp.url)
+    if(platform == "darwin"):
+      url = url.encode('latin1')
+    print(str(path)+' '+str(url))
+    local_filename = urllib2.urlparse.unquote(url.split('/')[-1])
+    file_size = resp.headers['Content-Length']
+    if(int(file_size)>=1024*1024*100):
+      while(1):
+        download = raw_input("The file is around {0}MB, still download?(y/n)".format(int(file_size)/1024/1024))
+        if(download.lower() == 'y'):
+          break
+        elif(download.lower() == 'n'):
+          return local_filename
+        else:
+          print("Please input only y or n!")
+    # NOTE the stream=True parameter
+    r = resp
+    with open(os.path.join(path, local_filename.decode('utf-8')), 'wb') as f:
+      for chunk in r.iter_content(chunk_size=1024): 
+        if chunk: # filter out keep-alive new chunks
+          f.write(chunk)
+              #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+  except Exception as inst:
+    caller = str(inspect.stack(0)[1][3])
+    inst = str(inst)
+    print(caller+'\t'+inst)
+    return 'error'
 
 def init_bb_session():
   sess = requests.session()
